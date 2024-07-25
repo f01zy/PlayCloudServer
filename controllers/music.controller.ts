581cd1b66baba12f06040ae63a1a2feb56ec9b37
f01zy <aminovali955@gmail.com>
2024-07-25 17:49:15 +0400

    commit

--- controllers/music.controller.ts
+++ controllers/music.controller.ts
@@ -115,7 +115,7 @@ export class MusicController {
       throw ApiError.BadRequest("Песни с таким id не существует")
     }
 
-    music.liked.indexOf(user.id) ? delete music.liked[user.id] : music.liked.push(user.id)
+    music.liked.indexOf(user.id) ? music.liked = music.liked.filter(userS => userS != user.id) : music.liked.push(user.id)
     music.save()
 
     return res.json(await musicService.populate(music))
