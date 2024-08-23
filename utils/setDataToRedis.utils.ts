9e5fc0ce4f61eab03ac30bea2ccca34cb2651e42
f01zy <aminovali955@gmail.com>
2024-08-23 23:12:21 +0400

    connect to redis

--- utils/setDataToRedis.utils.ts
+++ utils/setDataToRedis.utils.ts
@@ -1,3 +1,3 @@
 import { client } from ".."
 
-export const setDataToRedis = async (key: string, value: any) => { return await client.set(key, JSON.stringify(value)) }
+export const setDataToRedis = async (key: string, value: any) => { return await client.set(key, JSON.stringify(value), { EX: 60 * 5, NX: true }) }
