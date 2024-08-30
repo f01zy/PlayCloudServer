10e3243ee79e34085be5be00c3c134e82ed9a0d7
f01zy <aminovali955@gmail.com>
2024-08-31 01:47:36 +0400

    elastic search

--- service/elastic.service.ts
+++ service/elastic.service.ts
@@ -5,35 +5,15 @@ import { musicModel } from "../models/music.model";
 export class ElasticService {
   private readonly pageSize = 100
 
-  private async getMusic(page: number) {
-    const skip = (page - 1) * this.pageSize
-
-    const music = await musicModel.find()
-      .skip(skip)
-      .limit(this.pageSize)
-
-    return music;
-  }
-
   public async music() {
     let i = 0
-    let page = 1;
-    let count = 0;
 
-    do {
-      const data: Array<IMusic> = []
-      const musicArray = await this.getMusic(page)
+    const array = await musicModel.find()
 
-      for (const music of musicArray) { data.push(music) }
-
-      count = musicArray.length;
-
-      try {
-        const operations = data.flatMap(doc => [{ index: { _index: 'music' } }, doc])
-        await elastic.bulk({ refresh: true, operations }).then(res => console.log(`[INFO] music pack number ${i} is loaded, ${res.items.length} objects in ${musicArray.length}`))
-      } catch (error) { console.log(error) }
+    for (const music of array) {
+      await elastic.index({ document: music, index: "music" })
       i++
-      page++
-    } while (count === this.pageSize);
+      console.log(`[INFO] music \`${music._id}\` is loaded`)
+    }
   }
 }
